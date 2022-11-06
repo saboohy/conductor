1f1f90a8a6329b28266f5ef54ebdf7f558240e06
saboohy <sabuhi.alizada@yahoo.com>
2022-11-07 02:15:05 +0400

    Add methods

--- src/Collection.php
+++ src/Collection.php
@@ -2,7 +2,108 @@
 
 namespace Saboohy\Conductor;
 
-class Collection
+use Saboohy\Conductor\Collector;
+
+class Collection extends Collector
 {
-    
+    /**
+     * Controller getter
+     * 
+     * @param string $controller
+     * 
+     * @return void
+     */
+    protected function controller(string $controller = "") : void
+    {
+
+    }
+
+    /**
+     * Groups collections by controller
+     * 
+     * @param object|callable $callback
+     * 
+     * @return void
+     */
+    protected function group(object|callable $callback = null) : void
+    {
+
+    }
+
+    /**
+     * Prefix getter
+     * 
+     * @param string $prefix
+     * 
+     * @return void
+     */
+    protected function prefix(string $prefix = "") : void
+    {
+
+    }
+
+    /**
+     * Gets routing by GET method
+     * 
+     * @param string $uri
+     * @param string $action
+     * 
+     * @return void
+     */
+    protected function get(string $uri = "", string $action = "") : void
+    {
+
+    }
+
+    /**
+     * Gets routing by POST method
+     * 
+     * @param string $uri
+     * @param string $action
+     * 
+     * @return void
+     */
+    protected function post(string $uri = "", string $action = "") : void
+    {
+
+    }
+
+    /**
+     * Gets routing by PUT method
+     * 
+     * @param string $uri
+     * @param string $action
+     * 
+     * @return void
+     */
+    protected function put(string $uri = "", string $action = "") : void
+    {
+
+    }
+
+    /**
+     * Gets routing by PATCH method
+     * 
+     * @param string $uri
+     * @param string $action
+     * 
+     * @return void
+     */
+    protected function patch(string $uri = "", string $action = "") : void
+    {
+
+    }
+
+    /**
+     * Gets routing by DELETE method
+     * 
+     * @param string $uri
+     * @param string $action
+     * 
+     * @return void
+     */
+    protected function delete(string $uri = "", string $action = "") : void
+    {
+
+    }
 }
