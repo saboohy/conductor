38d26ecb274259fb469cc86d06780bc205cf8a10
saboohy <sabuhi.alizada@yahoo.com>
2022-11-07 02:25:10 +0400

    Add methods

--- src/Collector.php
+++ src/Collector.php
@@ -2,7 +2,76 @@
 
 namespace Saboohy\Conductor;
 
-abstract class Collector
+use Saboohy\Conductor\Interpreter;
+
+abstract class Collector extends Interpreter
 {
-    
+    /**
+     * Default prefix
+     * 
+     * @var string
+     */
+    private string $prefix = "/api";
+
+    /**
+     * Controller
+     * 
+     * @var string
+     */
+    private string $controller = "";
+
+    /**
+     * Collections
+     * 
+     * @var array
+     */
+    private array $routes = [];
+
+    /**
+     * Sets prefix
+     * 
+     * @param string $prefix
+     * 
+     * @return void
+     */
+    protected function setPrefix() : void
+    {
+
+    }
+
+    /**
+     * Sets controller
+     * 
+     * @param string $controller
+     * 
+     * @return void
+     */
+    protected function setController(string $controller = "") : void
+    {
+
+    }
+
+    /**
+     * Routing registerer
+     * 
+     * @param string $method
+     * @param string $uri
+     * @param string $action
+     * 
+     * @return void
+     */
+    protected function registerRouter(string $method = "GET", string $uri = "", string $action = "") : void
+    {
+
+    }
+
+    /**
+     * Returns collections
+     * 
+     * @return array
+     */
+    public function collections() : array
+    {
+        return $this->routes;
+    }
 }
