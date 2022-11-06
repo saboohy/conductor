853ad8f851545358fb9250c250970767042211d3
saboohy <sabuhi.alizada@yahoo.com>
2022-11-07 02:27:36 +0400

    Add AdminCollection for test

---
+++ tests/Collections/AdminCollection.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Saboohy\Conductor\Tests\Collections;
+
+use Saboohy\Conductor\Collection;
+
+class AdminCollection extends Collection
+{
+    /**
+     * @var string
+     */
+    protected string $prefix = "/admin";
+
+    /**
+     * Collects routes
+     * 
+     * @return void
+     */
+    public function collect() : void
+    {
+        $this->controller("ProductController")->group(function($product) {
+            $product->prefix("prefix");
+            $product->get("/", "index");
+            $product->post("/", "create");
+            $product->get("/{id}", "read");
+            $product->put("/{id}", "update");
+            $product->delete("/{id}", "delete");
+        });
+    }
+}
