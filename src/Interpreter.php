5b4912be092e02ab45a5f46ba565e9af1aa51a15
saboohy <sabuhi.alizada@yahoo.com>
2022-11-08 02:44:17 +0400

    Add initUri()

--- src/Interpreter.php
+++ src/Interpreter.php
@@ -2,7 +2,19 @@
 
 namespace Saboohy\Conductor;
 
+use function preg_match;
+use function preg_replace;
+
 abstract class Interpreter
 {
-    
+    protected function initUri(string $uri = "") : string
+    {
+        static $param = "/{[a-zA-Z]+}/";
+
+        return (
+            preg_match($param, $uri)
+            ? preg_replace($param, "/([a-zA-Z0-9-\._~!$&'\(\)\*\+,;=:@]+)/", $uri)
+            : $uri
+        );
+    }
 }
