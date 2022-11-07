f03d987f417c75816c93f5362847277606e89552
saboohy <sabuhi.alizada@yahoo.com>
2022-11-08 02:53:16 +0400

    Add initPrefix()

--- src/Interpreter.php
+++ src/Interpreter.php
@@ -7,6 +7,13 @@ use function preg_replace;
 
 abstract class Interpreter
 {
+    /**
+     * URI initializer
+     * 
+     * @param string $uri
+     * 
+     * @return string
+     */
     protected function initUri(string $uri = "") : string
     {
         static $param = "/{[a-zA-Z]+}/";
@@ -15,6 +22,22 @@ abstract class Interpreter
             preg_match($param, $uri)
             ? preg_replace($param, "/([a-zA-Z0-9-\._~!$&'\(\)\*\+,;=:@]+)/", $uri)
             : $uri
+        );
+    }
+
+    /**
+     * Prefix initializer
+     * 
+     * @param string $uri
+     * 
+     * @return string
+     */
+    protected function initPrefix(string $prefix = "") : string
+    {
+        return (
+            $prefix[0] !== "/" 
+            ? "/" . $prefix 
+            : $prefix
         );
     }
 }
