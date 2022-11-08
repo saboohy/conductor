2f1032735110a3e8f7695b093b0de1e12b85e1a0
saboohy <sabuhi.alizada@yahoo.com>
2022-11-08 23:22:35 +0400

    Add SiteCollection

---
+++ tests/Collections/SiteCollection.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Saboohy\Conductor\Tests\Collections;
+
+use Saboohy\Conductor\Collection;
+use Saboohy\Conductor\Contracts\CollectionContract;
+
+class SiteCollection extends Collection implements CollectionContract
+{
+    /**
+     * @var string
+     */
+    protected string $prefix = "";
+
+    /**
+     * Collects routes
+     * 
+     * @return void
+     */
+    public function collect() : void
+    {
+        $this->controller("CategoryController")->group(function($category) {
+            $category->prefix("category");
+            $category->get("/", "index");
+            $category->post("/", "create");
+            $category->get("/{id}", "read");
+            $category->put("/{id}", "update");
+            $category->delete("/{id}", "delete");
+        });
+    }
+}
